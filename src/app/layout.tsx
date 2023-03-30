b2b63b6c6a0196e40679bbc327cfd4c7e89aa791
neil.dev.ph <neil.dev.ph@tripleonetech.net>
2023-03-30 09:37:36 +0800

    Add title(metadata)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,10 @@ import Header from "@/components/Header";
 import Contact from "@/components/Contact";
 import Head from "next/head";
 
+export const metadata = {
+	title: "Neil Infante",
+};
+
 export default function RootLayout({
 	children,
 }: {
@@ -15,9 +19,6 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
-			<Head>
-				<title>Neil Infante</title>
-			</Head>
 			<body
 				className={`flex flex-col overscroll-none gap-y-4 h-full p-8 pt-6 py-14 text-charcoal ${raleway.className} max-w-4xl mx-auto overflow-auto`}
 			>
