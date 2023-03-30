4e6462df377208a91993eead47de987b65ee9310
neil.dev.ph <neil.dev.ph@tripleonetech.net>
2023-03-30 09:27:54 +0800

    add title?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ const raleway = Raleway({ subsets: ["latin"] });
 
 import Header from "@/components/Header";
 import Contact from "@/components/Contact";
+import Head from "next/head";
 
 export default function RootLayout({
 	children,
@@ -14,6 +15,9 @@ export default function RootLayout({
 }) {
 	return (
 		<html lang='en'>
+			<Head>
+				<title>Neil Infante</title>
+			</Head>
 			<body
 				className={`flex flex-col overscroll-none gap-y-4 h-full p-8 pt-6 py-14 text-charcoal ${raleway.className} max-w-4xl mx-auto overflow-auto`}
 			>
